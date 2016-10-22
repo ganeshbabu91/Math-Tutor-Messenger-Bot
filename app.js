0776e9b0baa548ff0b82608d909e0efd6f0bb7fd
ganeshbabu <ganeshbabu.atech@gmail.com>
2016-10-21 22:12:14 -0500

    added webhook

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-const PAGE_ACCESS_TOKEN = "EAANYlFHm1dcBAPftyw2cpm4zalz32qYxKGltr5cVOBKAl9IdxOG2MmjTEot5rNC39eKVD8FnFtFZB9w9w7BqvWGYmFhTFTAl1zlD7IaDg7hPjXrM5bc0qj9GOFDAxBG2HVZBxnuptfjvFUKgl3OrxoisZBBNCIz5lEv3JiVZBgZDZD";
-
+const 
+	PAGE_ACCESS_TOKEN = "EAANYlFHm1dcBAPftyw2cpm4zalz32qYxKGltr5cVOBKAl9IdxOG2MmjTEot5rNC39eKVD8FnFtFZB9w9w7BqvWGYmFhTFTAl1zlD7IaDg7hPjXrM5bc0qj9GOFDAxBG2HVZBxnuptfjvFUKgl3OrxoisZBBNCIz5lEv3JiVZBgZDZD",
+	VALIDATION_TOKEN = "verify-tbd";
+	
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 });
 
 app.post('/webhook', function (req, res) {

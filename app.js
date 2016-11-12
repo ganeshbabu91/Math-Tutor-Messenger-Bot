6b4095e029db8a7c19b214c7915c9d08aa201ce8
Ganeshbabu Thavasimuthu <ganeshbabu91@users.noreply.github.com>
2016-11-12 15:43:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const
 	bodyParser = require('body-parser')
   	request = require('request');
 	app = express();
-        app.use(express.static('public'));
 	port = process.env.PORT || 5000;
 	PAGE_ACCESS_TOKEN = "EAANYlFHm1dcBAGTy8ABwnPTa2ZBzm85elaLkEwI2YWgAf7gZBBVLcDWIUqbsnMJpNETIQUCJ72JUwf8BDqrhrZAH8ihbd3Ws3uDuqZB1Eehf0l5lJJ49qUB0RfVhIDw10Ns2ehSCeeTx8JtW6tjgKUG1Yk2Df9AZC63OIE2M7ygZDZD",
 	VALIDATION_TOKEN = "verify-tbd";
+
+app.use(express.static('public'));
 
 var algebra = require('algebra.js');
 
